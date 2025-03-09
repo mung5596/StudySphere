ed45c5b25b756051164f39b2dc20cf89bf27c65c
Jaeho Yoo <mung5596y@gmail.com>
2025-03-09 21:27:58 +0800

    Change formats

--- config/settings/base.py
+++ config/settings/base.py
@@ -131,6 +131,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 
+# Email functionality
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
