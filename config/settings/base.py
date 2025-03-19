3eb3411d4a16816daab434721304c4211c66e299
Jaeho Yoo <mung5596y@gmail.com>
2025-03-20 02:23:47 +0800

    Change TSL to SSL

--- config/settings/base.py
+++ config/settings/base.py
@@ -138,8 +138,8 @@ LOGOUT_REDIRECT_URL = "/"
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = "studyspheremanager@gmail.com"
 EMAIL_HOST_PASSWORD = "rmej ciyo dmrc ulca"
 DEFAULT_FROM_EMAIL = "studyspheremanager@gmail.com"
