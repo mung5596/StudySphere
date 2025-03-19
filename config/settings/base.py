a1b41961767c414b0a6d5e55374234a1cad4bcbe
Jaeho Yoo <mung5596y@gmail.com>
2025-03-19 20:37:42 +0800

    add comments

--- config/settings/base.py
+++ config/settings/base.py
@@ -128,10 +128,13 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+# Registration redirect url
+
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 
 # Email functionality
+
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
