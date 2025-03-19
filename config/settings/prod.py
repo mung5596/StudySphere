c41ef3c041d43f0cf08d1e314dc971e843d9ef04
Jaeho Yoo <mung5596y@gmail.com>
2025-03-20 01:58:48 +0800

    Change ALLOWED_HOSTS

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['54.254.110.78', 'studysphere2025.com']
+ALLOWED_HOSTS = ['13.213.128.146', 'studysphere2025.com']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = True
